fc4eaaa056c57ea4a06fff6e3521e0dddc8de9f0
newExpand <magin0auto@gmail.com>
2023-11-11 14:08:40 +0900

    :sparkles:feat: 로그인 회원가입 기능 추가

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -1,9 +1,15 @@
 "use client";
 
+import { useConvexAuth } from "convex/react";
+import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
+import { Spinner } from "@/components/spinner";
+import { SignInButton } from "@clerk/clerk-react";
 
 export const Heading = () => {
+    const { isAuthenticated, isLoading } = useConvexAuth();
+
     return (
         <div className="max-3-3xl space-y-4">
             <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
@@ -15,10 +21,27 @@ export const Heading = () => {
                 <br />
                 작업환경을 구성해 줍니다.
             </h3>
-            <Button>
-                시작하기
-                <ArrowRight className="w-4 h-4 ml-2" />
-            </Button>
+            {isLoading && (
+                <div className="w-full flex items-center justify-center">
+                    <Spinner size="lg" />
+                </div>
+            )}
+            {isAuthenticated && !isLoading && (
+                <Button asChild>
+                    <Link href="/documents">
+                        시작하기
+                        <ArrowRight className="w-4 h-4 ml-2" />
+                    </Link>
+                </Button>
+            )}
+            {!isAuthenticated && !isLoading && (
+                <SignInButton mode="modal">
+                    <Button>
+                        ExpandMemo 체험하기
+                        <ArrowRight className="w-4 h-4 ml-2" />
+                    </Button>
+                </SignInButton>
+            )}
         </div>
     );
 };
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+        {
+            domain: "https://together-earwig-99.clerk.accounts.dev",
+            applicationID: "convex",
+        },
+    ],
+};
