ea789e024256e6c41d0ca66894f247432088c0d8
newExpand <magin0auto@gmail.com>
2023-11-13 02:34:27 +0900

    :bug:fix: db에선 제거 되지만 이미지 업로드되는 곳에서는 제거 안되는 버그 수정

--- components/cover.tsx
+++ components/cover.tsx
@@ -9,6 +9,7 @@ import { Button } from "@/components/ui/button";
 import { useCoverImage } from "@/hooks/use-cover-image";
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
+import { useEdgeStore } from "@/lib/edgestore";
 
 interface CoverProps {
     url?: string;
@@ -16,11 +17,17 @@ interface CoverProps {
 }
 
 export const Cover = ({ url, preview }: CoverProps) => {
+    const { edgestore } = useEdgeStore();
     const params = useParams();
     const coverImage = useCoverImage();
     const removeCoverImage = useMutation(api.documents.removeCoverImage);
 
-    const onRemove = () => {
+    const onRemove = async () => {
+        if (url) {
+            await edgestore.publicFiles.delete({
+                url: url,
+            });
+        }
         removeCoverImage({
             id: params.documentId as Id<"documents">,
         });
@@ -45,7 +52,7 @@ export const Cover = ({ url, preview }: CoverProps) => {
             {url && !preview && (
                 <div className="opacity-0 group-hover:opacity-100 absolute bottom-5 right-5 flex items-center gap-x-2">
                     <Button
-                        onClick={coverImage.onOpen}
+                        onClick={() => coverImage.onReplace(url)}
                         className="text-muted-foreground text-xs"
                         variant="outline"
                         size="sm"
